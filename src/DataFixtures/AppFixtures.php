9be0e1cd2adb482b545983390e36af5355f96970
Safir Aniss Achour <achoursaphir@gmail.com>
2018-12-14 12:10:20 +0100

    Remove type in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,7 +5,6 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Role;
 use App\Entity\Status;
-use App\Entity\Type;
 
 class AppFixtures extends Fixture
 {
@@ -14,7 +13,6 @@ class AppFixtures extends Fixture
     {
         $this->loadRoles($manager);
         $this->loadStatus($manager);
-        $this->loadType($manager);
         $manager->flush();
     }
 
@@ -46,29 +44,5 @@ class AppFixtures extends Fixture
             $manager->persist($status);
         }
     }
-
-    public function loadType(ObjectManager $manager)
-    {
-        $typeList = [
-            [
-                'label'=>'League',
-                'description'=>'A competition in which each contestant meets all other contestants in turn.'
-            ],[
-                'label'=>'Knockout',
-                'description'=>'An elimination tournament'
-            ],[
-                'label'=>'Multistage',
-                'description'=>'A competition with Groupstage and finals'
-            ] 
-        ];
-
-        foreach ($typeList as $typeArray) {
-            $type = new Type();
-            $type->setLabel($typeArray['label']);
-            $type->setDescription($typeArray['description']);
-            $manager->persist($type);
-        }
-    }
-    
 }
 
